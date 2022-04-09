eb984bc89ad71716499c7316349558a787fd35b9
ClaudioFaraleski <faraleski@hotmail.com>
2022-04-09 17:35:45 -0300

    Aprenda Python na Pratica

---
+++ Aula-4/Tiposde Dados.py
@@ -0,0 +1,19 @@
+'''
+Nesse Modulo iremos aprender os tipos de dados
+
+str = Strings (' Curso de Python)
+int = inteiros ( 123, 0 , -896
+Float = ponto flutuante (50.10 , 98.2)
+bool = booleano (True/False)
+
+Segue alguns exemplos de tipos de dados
+
+Autor: Claudio Faraleski Junior
+
+'''
+
+print('Curso de Python',type('Curso de Python'))
+print(123,type(123))
+print(52,30,type(52.30))
+print(2==0,type(2 == 0))
+print(2 > 0 ,type(2 > 0))
