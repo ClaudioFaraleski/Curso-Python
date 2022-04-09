ad8df2ac8869b21c12c968a7a053655ce97778e9
ClaudioFaraleski <faraleski@hotmail.com>
2022-04-09 19:19:04 -0300

    Aprenda Python na Pratica

--- Basico/Aula-5/Casting.py
+++ Basico/Aula-5/Casting.py
@@ -2,7 +2,7 @@
 Nesse modulo vamos falar de Casting convertendo tipos de dados
 
 veja no exemplo abaixo criamos uma variavel e nomeamos ela com nome de texto
-e passamos um numerodo tipo  string  e  vamos converter o tipo dela  para inteiro
+e passamos um numero de  tipo  string  e  vamos converter o tipo dela  para inteiro
 
 usando esse exemplo podemos utilizar para converter outros tipos
 
