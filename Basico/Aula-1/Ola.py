227cca1f4c6030b1ab5eb1f20ddff8d69451ddbb
ClaudioFaraleski <faraleski@hotmail.com>
2022-04-09 18:08:25 -0300

    Aprenda Python na Pratica

--- Basico/Aula-1/Ola.py
+++ Basico/Aula-1/Ola.py
@@ -7,7 +7,7 @@ esse curso esta dividido em modulos do basico ,intermediario e avançado
 
 esse curso foi criado pora quem quer aprender a linguagem mais usada no mercado
 
- versão do Python Utilizado nesse curso sera a 3.8
+a versão do Python Utilizado nesse curso sera a 3.8
 
 o link abaixo e da documentação do python aonde voceis poderam tirar todas suas duvidas sobre a instalção do python
 
