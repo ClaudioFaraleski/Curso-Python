6db18626bd5f38b465a6ae199fbb54a48df81095
ClaudioFaraleski <faraleski@hotmail.com>
2022-04-09 18:48:15 -0300

    Aprenda Python na Pratica

--- Basico/Aula-1/Ola.py
+++ Basico/Aula-1/Ola.py
@@ -9,15 +9,24 @@ esse curso foi criado pora quem quer aprender a linguagem mais usada no mercado
 
 a versão do Python Utilizado nesse curso sera a 3.8
 
+você pode usar as mais diversas IDE para construir seus codigos mais utilizados é o Pycharm e Visual Studio Code
+segue os links
+
+# Pycharm: https://www.jetbrains.com/pt-br/pycharm/download/#section=linux
+# Visual Stduio Code: https://code.visualstudio.com/
+
+
 o link abaixo e da documentação do python aonde vocês poderam tirar todas suas duvidas sobre a instalação do python e
 demais conteudos como bibliotecas ,funções e muito mais não deixem de acessar.
+
+https://docs.python.org/3/
 
 "A maioria dos bons programadores fazem programação não porque eles esperam ser pagos ou obter Adulação pelo público,
 mas porque é divertido de programar."
 --
 Linus Torvalds
 
-https://docs.python.org/3/
+
 
 Autor : Claudio Faraleski Junior
 
