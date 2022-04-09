5b77b6dc7174840e185740e861e8cbbd614d0490
ClaudioFaraleski <faraleski@hotmail.com>
2022-04-09 17:36:17 -0300

    Aprenda Python na Pratica

--- Aula-4/Tiposde Dados.py
+++ Aula-4/Tiposde Dados.py
@@ -2,7 +2,7 @@
 Nesse Modulo iremos aprender os tipos de dados
 
 str = Strings (' Curso de Python)
-int = inteiros ( 123, 0 , -896
+int = inteiros ( 123, 0 , -896)
 Float = ponto flutuante (50.10 , 98.2)
 bool = booleano (True/False)
 
