51d3e579da63ae970e4a75ef19628fe2d98d4d83
ClaudioFaraleski <faraleski@hotmail.com>
2022-04-09 18:39:19 -0300

    Aprenda Python na Pratica

--- Basico/Aula-1/Ola.py
+++ Basico/Aula-1/Ola.py
@@ -9,7 +9,8 @@ esse curso foi criado pora quem quer aprender a linguagem mais usada no mercado
 
 a versão do Python Utilizado nesse curso sera a 3.8
 
-o link abaixo e da documentação do python aonde vocês poderam tirar todas suas duvidas sobre a instalação do python
+o link abaixo e da documentação do python aonde vocês poderam tirar todas suas duvidas sobre a instalação do python e
+demais conteudos como bibliotecas ,funções e muito mais não deixem de acessar.
 
 "A maioria dos bons programadores fazem programação não porque eles esperam ser pagos ou obter Adulação pelo público,
 mas porque é divertido de programar."
