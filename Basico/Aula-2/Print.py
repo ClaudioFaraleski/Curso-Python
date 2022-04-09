e0b0a482134108f9c618b808e42b34417f9078f4
ClaudioFaraleski <faraleski@hotmail.com>
2022-04-09 20:08:02 -0300

    Aprenda Python na Pratica

--- Basico/Aula-2/Print.py
+++ Basico/Aula-2/Print.py
@@ -2,19 +2,24 @@
 
 Nessa aula vamos definir a função print para imprimir o resultado na tela
 
-nesse modulo vamos usar tres metodos que iram imprimir o mesmo resultado
+existe tres metodos que você pode usar para imprimir o  resultado
+
+rode o programa e veja o resultado
+
+vamos ver na Pratica !!!
 
 Autor : Claudio Faraleski Junior
 '''
 
-
+# usando 1º metodo do print
 print('Seja bem vindo ao curso de Python')
 
-# usando outro metodo do print
-
+# usando 2º metodo do print
 mensagem = 'Python'
 print(f'Seja bem vindo ao curso de {mensagem}')
 
-# usando outro metodo .format
-
+# usando 3º metodo do print
 print('Seja bem vindo ao curso de {}'.format(mensagem))
+
+# Você observou que usando os 3 metodos o resultado foi igual para todos ao longo do curso iremos usar os 3 metodos
+# para que você consigar adquirir conhecimento. vamos praticar !!!
---
+++ Basico/Aula-2/funcao-print.py
@@ -0,0 +1,6 @@
+'''
+o que iremos aprender nesse modulo e sobre a função print
+
+A função print() é uma das funções mais importantes e usadas na linguagem Python. Sua função é, basicamente,
+exibir mensagens na tela ou enviá-las para outro dispositivo, como imprimir dentro de arquivos de texto.
+'''
